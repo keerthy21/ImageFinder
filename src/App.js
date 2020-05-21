ef0d853aa979cce25385dd400239f317491d08d0
powsokan <powsi1997@gmail.com>
2020-05-21 19:17:26 +0530

    search component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import NavBar from "./components/navbar/NavBar";
+import Search from "./components/search/Search";
 import "./App.css";
 import { MuiThemeProvider } from "material-ui/styles";
 
 function App() {
   return (
     <MuiThemeProvider>
+      <div>
       <NavBar />
+     <Search />
+      </div>
     </MuiThemeProvider>
   );
 }
