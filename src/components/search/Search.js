c65f2ebcd013d8522c971007dd19a6f11be11b8e
powsokan <powsi1997@gmail.com>
2020-05-21 20:00:47 +0530

    image component

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,39 +1,39 @@
-import React , {useState}from 'react'
+import React, {useState} from 'react'
 
-import { TextField , SelectField , MenuItem} from 'material-ui';
+import {TextField, SelectField, MenuItem} from 'material-ui';
 
 const Search = () => {
-     let [state, setstate] = useState();
-   state={
-       searchText:'',
-       amount:15,
-       apiUrl:'https://pixabay.com/api',
-       apiKey:'16671798-01f55fad2301ad3f652388cef',
-       images:[]
-   }
-   setstate(state);
+    const [state, setstate] = useState(
+        {
+            searchText: '',
+            amount: 15,
+            apiUrl: 'https://pixabay.com/api',
+            apiKey: '16671798-01f55fad2301ad3f652388cef',
+            images: []
+        }
+    );
     return (
         <div>
-          <TextField
-          name="SearchText"
-          value={this.state.searchText}  
-          onChange={this.onTextChange}
-          floatingLabelText="Search For Images"
-          fullWidth={true}
-          />
-          <br/>
-          <SelectField
-          name="amount"
-          value={this.state.amount}  
-          onChange={this.onAmountChange}
-          floatingLabelText="Amount"
-          >
-              <MenuItem value={5} primaryText="5" />
-              <MenuItem value={10} primaryText="10" />
-              <MenuItem value={15} primaryText="15" />
+            <TextField
+                name="SearchText"
+                value={this.state.searchText}
+                onChange={this.onTextChange}
+                floatingLabelText="Search For Images"
+                fullWidth={true}
+            />
+            <br/>
+            <SelectField
+                name="amount"
+                value={this.state.amount}
+                onChange={this.onAmountChange}
+                floatingLabelText="Amount"
+            >
+                <MenuItem value={5} primaryText="5"/>
+                <MenuItem value={10} primaryText="10"/>
+                <MenuItem value={15} primaryText="15"/>
 
-          </SelectField>
-          <br/>
+            </SelectField>
+            <br/>
         </div>
     );
 }
