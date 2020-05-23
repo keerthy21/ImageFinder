a33c706380d232d9d834da2b325c42a174712540
powsokan <powsi1997@gmail.com>
2020-05-23 08:31:47 +0530

    api call

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,33 +1,30 @@
-import React, {useEffect, useState} from 'react';
+import React, {Component} from 'react';
 import {TextField, SelectField, MenuItem} from 'material-ui';
 import axios from 'axios';
 
 
-const Search = () => {
+class Search extends Component {
+    state = {
+        searchText: '',
+        amount: 15,
+        apiUrl: 'https://pixabay.com/api',
+        apiKey: '16671798-01f55fad2301ad3f652388cef',
+        images: []
+    }
 
-    const [images, setImages] = useState([]);
-    const [searchText, setSearchText] = useState('');
-    const [amount, setAmount] = useState(15);
-
-    useEffect(() => {
-       onTextChange()
-    }, [searchText]);
-
-    useEffect(() => {
-      onAmountChange()
-    }, [amount]);
-
-    const onTextChange = e => {
+    const
+    onTextChange = e => {
         setSearchText(e.target.value);
     };
 
-    const onAmountChange = e => {
+    const
+    onAmountChange = e => {
         setAmount(e.target.value);
     };
 
-    const apiCall = () => {
-        const apiUrl = 'https://pixabay.com/api'
-        const apiKey = '16671798-01f55fad2301ad3f652388cef'
+    const
+    apiCall = () => {
+
         axios.get(`${apiUrl}/?key=${apiKey}&q=${searchText}&image_type=photo&per_page=${amount}&safesearch=true`)
             .then(res => setImages(res.data.hits))
             .catch(err => console.log(err));
@@ -36,30 +33,32 @@ const Search = () => {
     };
 
 
-    return (
-        <div>
-            <TextField
-                name="SearchText"
-                value={searchText}
-                onChange={onTextChange}
-                floatingLabelText="Search For Images"
-                fullWidth={true}
-            />
-            <br/>
-            <SelectField
-                name="amount"
-                value={amount}
-                onChange={onAmountChange}
-                floatingLabelText="Amount"
-            >
-                <MenuItem value={5} primaryText="5"/>
-                <MenuItem value={10} primaryText="10"/>
-                <MenuItem value={15} primaryText="15"/>
+    return(
 
-            </SelectField>
-            <br/>
-        </div>
-    );
+<div>
+<TextField
+name="SearchText"
+value={searchText}
+onChange={onTextChange}
+floatingLabelText="Search For Images"
+fullWidth={true}
+/>
+<br/>
+<SelectField
+name="amount"
+value={amount}
+onChange={onAmountChange}
+floatingLabelText="Amount"
+>
+<MenuItem value={5} primaryText="5"/>
+<MenuItem value={10} primaryText="10"/>
+<MenuItem value={15} primaryText="15"/>
+
+</SelectField>
+<br/>
+</div>
+)
+    ;
 }
 
 export default Search
