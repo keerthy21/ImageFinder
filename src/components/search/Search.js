609c63e5982693d684cebe17ef36761f5ee0bd84
powsokan <powsi1997@gmail.com>
2020-05-23 09:18:55 +0530

    api call

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -12,40 +12,44 @@ class Search extends Component {
     };
 
     onTextChange = e => {
-        this.setState({ [e.target.name]: e.target.value });
+        this.setState({[e.target.name]: e.target.value}, () => {
+            axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
+                .then(res => this.setState({images: res.data.hits}))
+                .catch(err => console.log(err));
+        });
     };
 
-    onAmountChange = e =>{
-        this.setState({})
+    onAmountChange = e => {
+        this.setState({[e.target.name]:e.target.value})
     }
 
-render() {
-    console.log(this.state.images);
-    return(
-        <div>
-            <TextField
-                name="SearchText"
-                floatingLabelText="Search For Images"
-                value={this.state.searchText}
-                onChange={this.onTextChange}
-                fullWidth={true}
-            />
-            <br/>
-            <SelectField
-                name="amount"
-                floatingLabelText="Amount"
-                value={this.state.amount}
-                onChange={this.onAmountChange}
-            >
-                <MenuItem value={5} primaryText="5"/>
-                <MenuItem value={10} primaryText="10"/>
-                <MenuItem value={15} primaryText="15"/>
+    render() {
+        console.log(this.state.images);
+        return (
+            <div>
+                <TextField
+                    name="SearchText"
+                    floatingLabelText="Search For Images"
+                    value={this.state.searchText}
+                    onChange={this.onTextChange}
+                    fullWidth={true}
+                />
+                <br/>
+                <SelectField
+                    name="amount"
+                    floatingLabelText="Amount"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                >
+                    <MenuItem value={5} primaryText="5"/>
+                    <MenuItem value={10} primaryText="10"/>
+                    <MenuItem value={15} primaryText="15"/>
 
-            </SelectField>
-            <br/>
-        </div>
-    );
-}
+                </SelectField>
+                <br/>
+            </div>
+        );
+    }
 }
 
 export default Search;
