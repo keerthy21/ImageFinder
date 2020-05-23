99e7c1d240e9a2cd0a305860750a1d9c05d9a89a
powsokan <powsi1997@gmail.com>
2020-05-23 10:02:03 +0530

    image result

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -14,9 +14,13 @@ class Search extends Component {
 
     onTextChange = (e, value) => {
         this.setState({searchText: value}, () => {
+            if (value===''){
+                this.setState({images:[]});
+            }else {
             axios.get(`${this.state.apiUrl}/?key=${this.state.apiKey}&q=${this.state.searchText}&image_type=photo&per_page=${this.state.amount}&safesearch=true`)
                 .then(res => this.setState({images: res.data.hits}))
                 .catch(err => console.log(err));
+            }
         });
     };
 
