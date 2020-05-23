e17f9d62e20f092e5f8d75c7fb0b983e88184faa
powsokan <powsi1997@gmail.com>
2020-05-23 09:51:24 +0530

    image result

--- src/components/image-result/ImageResult.js
+++ src/components/image-result/ImageResult.js
@@ -1,42 +1,43 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import {GridList,GridTile,IconButton,ZoomIn,Dialog,FlatButton} from "material-ui";
+import {GridList, GridTile, IconButton, ZoomIn, Dialog, FlatButton} from "material-ui";
 
 
 class ImageResult extends Component {
     render() {
         let imageListContent;
-        const {images}=this.props;
+        const {images} = this.props;
 
-        if (images){
+        if (images) {
             imageListContent = {
-             <GridList cols={3}>
-                 {images.map(img =>(
+            <GridList cols={3}>
+                {images.map(img => (
                     <GridTile
                         title={img.tags}
                         key={img.id}
                         subtitle={<span>by <strong>{img.user}</strong></span>}
                         actionIcon={<IconButton>
-                            <ZoomIn  color={white} />
+                            <ZoomIn color="white"/>
                         </IconButton>}
                     >
 
                     </GridTile>
-                 ))}
-             </GridList>
-            }
-        }else {
+                ))}
+            </GridList>
+        }
+        } else {
             imageListContent = null;
         }
 
         return (
             <div>
-              
+
             </div>
         );
     }
 }
-ImageResult.propTypes ={
+
+ImageResult.propTypes = {
     images: PropTypes.array.isRequired
 }
 export default ImageResult;
